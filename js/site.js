6c899c450ca6b35bd2b4c54ad0beb97f251f18d5
Bruce Van Horn <brucevanhorn@me.com>
2023-09-04 20:48:38 -0500

    initial commit

---
+++ js/site.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+  // handler for the nav burger
+  $(".navbar-burger").click(function() {
+    $(".navbar-burger").toggleClass("is-active");
+    $(".navbar-menu").toggleClass("is-active");
+
+  });
+});
